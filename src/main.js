b361666021937dbe734c419199af8a7063d84173
ahmadraza-github <mahmadraza1525@gmail.com>
2026-07-13 22:47:47 +0500

    2 sections left

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
 import './style.css';
+
+let cards = document.querySelectorAll('.card');
+let show_cards = document.querySelectorAll('.work-show-card');
+cards.forEach((card, index) => {
+    card.addEventListener('click', () => {
+        show_cards.forEach((show_card, j) => {
+            if(index === j) {
+                show_card.classList.add('block');
+                show_card.classList.remove('hidden');
+            } else {
+                show_card.classList.add('hidden');
+                show_card.classList.remove('block');
+            }
+        });
+    })
+});
